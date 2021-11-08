92d6ca1cb20f4c4bdb2814857c8985900bba2ac7
cường lê <vancuong.tkt36@gmail.com>
2021-11-08 12:56:11 +0700

    Đã đẩy

---
+++ Du_An_1_QL_BAN_GIAY/3_GUI_PresentationLayer/Frm_Main.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace _3_GUI_PresentationLayer
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Main));
+            this.pnl_MenuLeft = new System.Windows.Forms.Panel();
+            this.fip_Menuleft = new System.Windows.Forms.FlowLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_main = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pnl_MenuRight = new System.Windows.Forms.Panel();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btn_help = new System.Windows.Forms.Button();
+            this.btn_min = new System.Windows.Forms.Button();
+            this.pnl_Top = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_tieude = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnl_MenuLeft.SuspendLayout();
+            this.fip_Menuleft.SuspendLayout();
+            this.pnl_MenuRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.pnl_Top.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_MenuLeft
+            // 
+            this.pnl_MenuLeft.BackColor = System.Drawing.Color.Gray;
+            this.pnl_MenuLeft.Controls.Add(this.fip_Menuleft);
+            this.pnl_MenuLeft.Controls.Add(this.pnl_MenuRight);
+            this.pnl_MenuLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_MenuLeft.ForeColor = System.Drawing.Color.White;
+            this.pnl_MenuLeft.Location = new System.Drawing.Point(0, 67);
+            this.pnl_MenuLeft.Name = "pnl_MenuLeft";
+            this.pnl_MenuLeft.Size = new System.Drawing.Size(452, 934);
+            this.pnl_MenuLeft.TabIndex = 0;
+            // 
+            // fip_Menuleft
+            // 
+            this.fip_Menuleft.Controls.Add(this.button4);
+            this.fip_Menuleft.Controls.Add(this.btn_main);
+            this.fip_Menuleft.Controls.Add(this.button6);
+            this.fip_Menuleft.Controls.Add(this.label2);
+            this.fip_Menuleft.Controls.Add(this.button5);
+            this.fip_Menuleft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fip_Menuleft.Location = new System.Drawing.Point(0, 301);
+            this.fip_Menuleft.Name = "fip_Menuleft";
+            this.fip_Menuleft.Size = new System.Drawing.Size(452, 633);
+            this.fip_Menuleft.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Image = global::_3_GUI_PresentationLayer.Properties.Resources.xxx;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(3, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(397, 53);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "Quản lý nhân viên";
+            this.button4.Text = "Quản lý nhân viên";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.MenuClick);
+            // 
+            // btn_main
+            // 
+            this.btn_main.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_main.FlatAppearance.BorderSize = 0;
+            this.btn_main.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            this.btn_main.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_main.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_main.Image = global::_3_GUI_PresentationLayer.Properties.Resources.xxx;
+            this.btn_main.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_main.Location = new System.Drawing.Point(3, 62);
+            this.btn_main.Name = "btn_main";
+            this.btn_main.Size = new System.Drawing.Size(397, 53);
+            this.btn_main.TabIndex = 4;
+            this.btn_main.Tag = "Main";
+            this.btn_main.Text = "Main";
+            this.btn_main.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_main.UseVisualStyleBackColor = false;
+            this.btn_main.Click += new System.EventHandler(this.MenuClick);
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Image = global::_3_GUI_PresentationLayer.Properties.Resources.xxx;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(3, 121);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(397, 53);
+            this.button6.TabIndex = 7;
+            this.button6.Tag = "Thống kê";
+            this.button6.Text = "Thống kê";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.MenuClick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(3, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(397, 2);
+            this.label2.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Image = global::_3_GUI_PresentationLayer.Properties.Resources.xxx;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(3, 182);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(397, 53);
+            this.button5.TabIndex = 6;
+            this.button5.Tag = "Quản lý sản phẩm";
+            this.button5.Text = "Quản lý sản phẩm";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.MenuClick);
+            // 
+            // pnl_MenuRight
+            // 
+            this.pnl_MenuRight.Controls.Add(this.lbl_user);
+            this.pnl_MenuRight.Controls.Add(this.pictureBox4);
+            this.pnl_MenuRight.Controls.Add(this.btn_help);
+            this.pnl_MenuRight.Controls.Add(this.btn_min);
+            this.pnl_MenuRight.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_MenuRight.Location = new System.Drawing.Point(0, 0);
+            this.pnl_MenuRight.Name = "pnl_MenuRight";
+            this.pnl_MenuRight.Size = new System.Drawing.Size(452, 301);
+            this.pnl_MenuRight.TabIndex = 2;
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_user.Location = new System.Drawing.Point(222, 210);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(138, 55);
+            this.lbl_user.TabIndex = 6;
+            this.lbl_user.Text = "label1";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(57, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(395, 165);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btn_help
+            // 
+            this.btn_help.FlatAppearance.BorderSize = 0;
+            this.btn_help.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_help.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_help.Location = new System.Drawing.Point(-3, 65);
+            this.btn_help.Name = "btn_help";
+            this.btn_help.Size = new System.Drawing.Size(54, 53);
+            this.btn_help.TabIndex = 3;
+            this.btn_help.Tag = "Trợ giúp";
+            this.btn_help.Text = "?";
+            this.btn_help.UseVisualStyleBackColor = true;
+            this.btn_help.Click += new System.EventHandler(this.MenuClick);
+            // 
+            // btn_min
+            // 
+            this.btn_min.FlatAppearance.BorderSize = 0;
+            this.btn_min.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_min.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_min.Location = new System.Drawing.Point(0, 3);
+            this.btn_min.Name = "btn_min";
+            this.btn_min.Size = new System.Drawing.Size(54, 53);
+            this.btn_min.TabIndex = 2;
+            this.btn_min.Text = "☰";
+            this.btn_min.UseVisualStyleBackColor = true;
+            this.btn_min.Click += new System.EventHandler(this.btn_min_Click);
+            // 
+            // pnl_Top
+            // 
+            this.pnl_Top.Controls.Add(this.label1);
+            this.pnl_Top.Controls.Add(this.lbl_tieude);
+            this.pnl_Top.Controls.Add(this.pictureBox2);
+            this.pnl_Top.Controls.Add(this.pictureBox1);
+            this.pnl_Top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_Top.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Top.Name = "pnl_Top";
+            this.pnl_Top.Size = new System.Drawing.Size(1853, 67);
+            this.pnl_Top.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1721, 2);
+            this.label1.TabIndex = 2;
+            // 
+            // lbl_tieude
+            // 
+            this.lbl_tieude.AutoSize = true;
+            this.lbl_tieude.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_tieude.Location = new System.Drawing.Point(3, 3);
+            this.lbl_tieude.Name = "lbl_tieude";
+            this.lbl_tieude.Size = new System.Drawing.Size(123, 55);
+            this.lbl_tieude.TabIndex = 5;
+            this.lbl_tieude.Text = "Main";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1721, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(66, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = global::_3_GUI_PresentationLayer.Properties.Resources.xxx;
+            this.pictureBox1.Location = new System.Drawing.Point(1787, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1853, 1001);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnl_MenuLeft);
+            this.Controls.Add(this.pnl_Top);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Frm_Login_Load);
+            this.pnl_MenuLeft.ResumeLayout(false);
+            this.fip_Menuleft.ResumeLayout(false);
+            this.pnl_MenuRight.ResumeLayout(false);
+            this.pnl_MenuRight.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.pnl_Top.ResumeLayout(false);
+            this.pnl_Top.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_MenuLeft;
+      
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_main;
+        private System.Windows.Forms.Panel pnl_MenuRight;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btn_help;
+        private System.Windows.Forms.Button btn_min;
+        private System.Windows.Forms.Panel pnl_Top;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_tieude;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.FlowLayoutPanel fip_Menuleft;
+    }
+}
+
---
+++ Du_An_1_QL_BAN_GIAY/3_GUI_PresentationLayer/Frm_Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_GUI_PresentationLayer
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Login_Load(object sender, EventArgs e)
+        {
+            btn_main.PerformClick();
+            foreach (Control x in fip_Menuleft.Controls)
+                x.Width = fip_Menuleft.Width;
+             
+        }
+        private void MenuClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            lbl_tieude.Text = btn.Tag.ToString();
+            foreach (Control x  in fip_Menuleft.Controls)
+            {
+                x.BackColor = fip_Menuleft.BackColor;
+                if (btn.Name!=btn_help.Name)
+                {
+                    btn_help.BackColor = fip_Menuleft.BackColor;
+                }
+                btn.BackColor = Color.DodgerBlue;
+            }
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+     
+
+        private void btn_min_Click(object sender, EventArgs e)
+        {
+            pnl_MenuLeft.Width = (pnl_MenuLeft.Width == 452) ? 55 : 452;
+        }
+    }
+}
