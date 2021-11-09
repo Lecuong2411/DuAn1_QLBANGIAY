bb35650bc3980e9a7f69fba1241826402d2f84d2
tuyenlhph15504 <tuyenlhph15504@fpt.edu.vn>
2021-11-09 22:29:05 +0700

    Signed-off-by: tuyenlhph15504 <tuyenlhph15504@fpt.edu.vn> update thêm dữ liệu vào lớp bus danh sách hóa đơn bán hàng

--- Du_An_1_QL_BAN_GIAY/2_BUS_BusinessLayer/IService/IBUS_DanhSachHoaDonBanHang.cs
+++ Du_An_1_QL_BAN_GIAY/2_BUS_BusinessLayer/IService/IBUS_DanhSachHoaDonBanHang.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _2_BUS_BusinessLayer.Models;
 
 namespace _2_BUS_BusinessLayer
 {
-    interface IBUS_DanhSachHoaDonBanHang
+    public interface IBUS_DanhSachHoaDonBanHang
     {
+        public List<Danhsachhoadonbanhang> ListdDanhsachhoadonbanhang();
+        //dùng để đẩy dữ liệu của 3 bảng nhân viên , khách hàng , hóa đơn 
+        public void loatdata();
     }
 }
--- Du_An_1_QL_BAN_GIAY/2_BUS_BusinessLayer/Models/Danhsachhoadonbanhang.cs
+++ Du_An_1_QL_BAN_GIAY/2_BUS_BusinessLayer/Models/Danhsachhoadonbanhang.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _1_DAL_DataAccessLayer.Models;
 
 namespace _2_BUS_BusinessLayer.Models
 {
-    class Danhsachhoadonbanhang
+    public class Danhsachhoadonbanhang
     {
+        public HoaDon hoadon { get; set; }
+        public  KhachHang KhachHang { get; set; }
+        public  NhanVien NhanVien { get; set; }
+
+        public Danhsachhoadonbanhang()
+        {
+            hoadon = new HoaDon();
+            KhachHang = new KhachHang();
+            NhanVien = new NhanVien();
+        }
+
+        public Danhsachhoadonbanhang(HoaDon hoadon, KhachHang khachHang, NhanVien nhanVien)
+        {
+            this.hoadon = hoadon;
+            KhachHang = khachHang;
+            NhanVien = nhanVien;
+        }
     }
 }
