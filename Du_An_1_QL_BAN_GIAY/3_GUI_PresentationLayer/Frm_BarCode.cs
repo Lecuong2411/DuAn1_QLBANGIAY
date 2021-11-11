df73c1ea84920f37ec2f1ea529e9b3ccc126051c
user2810 <kienntph13749@fpt.edu.vn>
2021-11-11 21:59:03 +0700

    d

---
+++ Du_An_1_QL_BAN_GIAY/3_GUI_PresentationLayer/Frm_BarCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace _3_GUI_PresentationLayer
+{
+    public partial class Frm_BarCode : Form
+    {
+      
+        public Frm_BarCode()
+        {
+            InitializeComponent();
+         load();    
+        }
+
+        private void btn_thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void load()
+        {
+            BarcodeWriter brCode = new BarcodeWriter()
+            {
+                Format = BarcodeFormat.CODE_128
+            };
+            pictureBox1.Image = brCode.Write(Frm_sanpham._barcode);
+        }
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog fileDialog = new SaveFileDialog() { Filter = "PNG|*.png"})
+            {
+                if (fileDialog.ShowDialog()==DialogResult.Yes)
+                {
+                    pictureBox1.Image.Save(fileDialog.FileName);
+                }   
+            }
+        }
+    }
+}
