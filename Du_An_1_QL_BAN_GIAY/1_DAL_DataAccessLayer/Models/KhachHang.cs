51ac4f6848606f80c4afc68b0ad3918dc48b2aa3
cường lê <vancuong.tkt36@gmail.com>
2021-11-13 17:13:45 +0700

    r

---
+++ Du_An_1_QL_BAN_GIAY/1_DAL_DataAccessLayer/Models/KhachHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace _1_DAL_DataAccessLayer.Models
+{
+    [Table("KhachHang")]
+    public partial class KhachHang
+    {
+        public KhachHang()
+        {
+            HoaDons = new HashSet<HoaDon>();
+        }
+
+        [Key]
+        [Column("MaKH")]
+        public Guid MaKh { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string TenKh { get; set; }
+        [Required]
+        [Column("SDT")]
+        [StringLength(12)]
+        public string Sdt { get; set; }
+
+        [InverseProperty(nameof(HoaDon.MaKhNavigation))]
+        public virtual ICollection<HoaDon> HoaDons { get; set; }
+    }
+}
